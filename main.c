eda504673d8e355d936ac7df6f424c8306aec53d
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-06-01 00:19:35 -0300

    indentando

--- main.c
+++ main.c
@@ -22,82 +22,82 @@ void get_haircut();
 void giveup_haircut();
 
 int main() {
-int i;
-srand(time(NULL));
-for(i=0; i<BARBERS; i++)
-{
-	sem_init(&customers[i], 1, 0);
-}
-for(i=0; i<BARBERS; i++)
-	sem_init(&barbers[i], 1, 0);
-sem_init(&mutex, 1, 1);
+	int i;
+	srand(time(NULL));
+	for(i=0; i<BARBERS; i++)
+	{
+		sem_init(&customers[i], 1, 0);
+	}
+	for(i=0; i<BARBERS; i++)
+		sem_init(&barbers[i], 1, 0);
+	sem_init(&mutex, 1, 1);
 
-pthread_t b[BARBERS], c;
+	pthread_t b[BARBERS], c;
 
- /* criando BARBERS barbeiros */
-for(i=0; i<BARBERS; i++)
-{
-	int j=i;
-	pthread_create(&b[i], NULL, (void *) barber, (void*)&j);
-	sleep(1); //única parte ruim do codigo, para dar tempo do barbeiro utilizar o j
-	//possivel mudança : adicionar um mutex relativo ao j
-	//porém todas as vezes que testei, dava problemas, então mantive assim
-}
+ 	/* criando BARBERS barbeiros */
+	for(i=0; i<BARBERS; i++)
+	{
+		int j=i;
+		pthread_create(&b[i], NULL, (void *) barber, (void*)&j);
+		sleep(1); //única parte ruim do codigo, para dar tempo do barbeiro utilizar o j
+		//possivel mudança : adicionar um mutex relativo ao j
+		//porém todas as vezes que testei, dava problemas, então mantive assim
+	}
 
  /* criação indefinida de clientes */
-while(1) {
-	pthread_create(&c, NULL, (void *) customer, NULL);
-	sleep(1);
-}
+	while(1) {
+		pthread_create(&c, NULL, (void *) customer, NULL);
+		sleep(1);
+	}
 
-return 0;
+	return 0;
 }
 
 void* barber(void *arg) {
-int i = *((int*)arg);
-while(1) {
-sem_wait(&customers[i]);   /* vai dormir se o número de clientes for 0 */
-sem_wait(&mutex);       /* obtém acesso a 'waiting' */
-waiting = waiting - 1;  /*descresce de um o contador de clientes à espera */
-sem_post(&barbers[i]);     /* um barbeiro está agora pronto para cortar cabelo */
-sem_post(&mutex);       /* libera 'waiting' */
-cut_hair(i);             /* corta o cabelo (fora da região crítica) */
-}
+	int i = *((int*)arg);
+	while(1) {
+		sem_wait(&customers[i]);   /* vai dormir se o número de clientes for 0 */
+		sem_wait(&mutex);       /* obtém acesso a 'waiting' */
+		waiting = waiting - 1;  /*descresce de um o contador de clientes à espera */
+		sem_post(&barbers[i]);     /* um barbeiro está agora pronto para cortar cabelo */
+		sem_post(&mutex);       /* libera 'waiting' */
+		cut_hair(i);             /* corta o cabelo (fora da região crítica) */
+	}
 
-pthread_exit(NULL);
+	pthread_exit(NULL);
 }
 
 void* customer(void *arg) {
-int whoINeed = rand()%BARBERS;
-sem_wait(&mutex);           /* entra na região crítica */
-if(waiting < CHAIRS) {      /* se não houver cadeiras vazias, saia */
-customer_arrived();
-waiting = waiting + 1;  /* incrementa o contador de clientes à espera */
-sem_post(&customers[whoINeed]);   /* acorda o barbeiro se necessário */
-sem_post(&mutex);       /* libera o acesso a 'waiting' */
-sem_wait(&barbers[whoINeed]);     /* vai dormir se o número de barbeiros livres for 0 */
-get_haircut();          /* sentado e sendo servido */
-} else {
-sem_post(&mutex);       /* a barbearia está cheia; não espera */
-giveup_haircut();
+	int whoINeed = rand()%BARBERS;
+	sem_wait(&mutex);           /* entra na região crítica */
+	if(waiting < CHAIRS) {      /* se não houver cadeiras vazias, saia */
+		customer_arrived();
+		waiting = waiting + 1;  /* incrementa o contador de clientes à espera */
+		sem_post(&customers[whoINeed]);   /* acorda o barbeiro se necessário */
+		sem_post(&mutex);       /* libera o acesso a 'waiting' */
+		sem_wait(&barbers[whoINeed]);     /* vai dormir se o número de barbeiros livres for 0 */
+		get_haircut();          /* sentado e sendo servido */
+	} else {
+		sem_post(&mutex);       /* a barbearia está cheia; não espera */
+		giveup_haircut();
 
-}
+	}
 
-pthread_exit(NULL);
+	pthread_exit(NULL);
 }
 
 void cut_hair(int id) {
-printf("Barbeiro %d estah cortando o cabelo de alguem!\n",id);
-sleep(3);
+	printf("Barbeiro %d estah cortando o cabelo de alguem!\n",id);
+	sleep(3);
 }
 
 void customer_arrived() {
-printf("Cliente chegou para cortar cabelo!\n");
+	printf("Cliente chegou para cortar cabelo!\n");
 }
 void get_haircut() {
-printf("Cliente estah tendo o cabelo cortado!\n");
+	printf("Cliente estah tendo o cabelo cortado!\n");
 }
 
 void giveup_haircut() {
-printf("Cliente desistiu! (O salao estah muito cheio!)\n");
+	printf("Cliente desistiu! (O salao estah muito cheio!)\n");
 }
